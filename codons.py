587871d813729cb8ddc2d25fdd6d47c9d3088ade
325197077 <rontalshaked@gmail.com>
2024-12-10 18:30:51 +0200

    Update codons.py

--- codons.py
+++ codons.py
@@ -1,4 +1,15 @@
 def create_codon_dict(file_path):
-    pass # Replace the pass with your code
+  file = open(file_path)
+  rows = file.readlines()
+  file.close()
+  file_path = "data/codons.txt"
+
+  shaked_dict= {}
+  for row in rows:
+    row= row.strip().split()
+    shaked_dict [row[0]]= row[2]
+  print(shaked_dict)
+
+  return shaked_dict
 
 
